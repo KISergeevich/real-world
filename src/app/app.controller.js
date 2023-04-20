5bbe1ff4d1b98c9ff3e87b38d9868f61e8b74b61
Kalinin Ivan <indimion57@gmail.com>
2023-04-20 16:37:44 +0300

    add event change states

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -9,6 +9,30 @@ export class AppController {
         this.model = new AppModel();
 
         this.view = new AppView(element);
+
+        this.view.subscribeSwitchToSignup(this.onSwitchToSignup.bind(this));
+        this.view.subscribeSwitchToLogin(this.onSwitchToLogin.bind(this));
+        this.view.subscribeSwitchToLogout(this.onSwitchToLogout.bind(this));
+
+
+        this.view.refresh(this.model);
+
+    }
+
+    onSwitchToSignup() {
+        this.model.state = 'signup';
         this.view.refresh(this.model);
     }
+
+    onSwitchToLogin() {
+        this.model.state = "login";
+        this.view.refresh(this.model);
+    }
+
+    onSwitchToLogout() {
+        this.model.token = undefined;
+        this.model.state = "login";
+        this.view.refresh(this.model);
+    }
+
 }
--- src/app/app.view.js
+++ src/app/app.view.js
@@ -3,6 +3,10 @@ import { newElement } from "../utils/new-element";
 
 export class AppView {
     element;
+    switchToSignupCallback;
+    switchToLoginCallback;
+    switchToLogoutCallback;
+
 
     constructor(element) {
         this.element = element;
@@ -20,25 +24,25 @@ export class AppView {
         const header = newElement("section", "header");
         if (model.state === 'signup') {
             const login = newElement("button", "header__login", "login");
-            // login.addEventListener("click", () => {
-            //     this.emitSwitchToLogin();
-            // });
+            login.addEventListener("click", () => {
+                this.emitSwitchToLogin();
+            });
             header.append(login);
         }
 
         if (model.state === 'login') {
             const signup = newElement("button", "header__signup", "signup");
-            // signup.addEventListener("click", () => {
-            //     this.emitSwitchToSignup();
-            // });
+            signup.addEventListener("click", () => {
+                this.emitSwitchToSignup();
+            });
             header.append(signup);
         }
 
         if (model.state === 'auth') {
             const logout = newElement("button", "header__logout", "logout");
-            // logout.addEventListener("click", () => {
-            //     this.emitSwitchToLogout();
-            // });
+            logout.addEventListener("click", () => {
+                this.emitSwitchToLogout();
+            });
             header.append(logout);
         }
         return header;
@@ -48,4 +52,36 @@ export class AppView {
         const main = newElement("section", "main", model.state);
         return main;
     }
+
+    subscribeSwitchToSignup(callback) {
+        this.switchToSignupCallback = callback;
+    }
+
+    emitSwitchToSignup() {
+        if (this.switchToSignupCallback !== undefined) {
+            this.switchToSignupCallback();
+        }
+    }
+
+    subscribeSwitchToLogin(callback) {
+        this.switchToLoginCallback = callback;
+    }
+
+    emitSwitchToLogin() {
+        if (this.switchToLoginCallback !== undefined) {
+            this.switchToLoginCallback();
+        }
+    }
+
+    subscribeSwitchToLogout(callback) {
+        this.switchToLogoutCallback = callback;
+    }
+
+    emitSwitchToLogout() {
+        if (this.switchToLogoutCallback !== undefined) {
+            this.switchToLogoutCallback();
+        }
+    }
+
+
 }
